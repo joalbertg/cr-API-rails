77a703dbe89c7919c0aaf9294d96c440b8ea4f12
lisset-orozco <liss612@gmail.com>
2019-03-31 14:07:51 -0600

    user_provider_uid from_omniauth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,32 @@ RSpec.describe User, type: :model do
     it { should_not allow_value('liss@co-digo').for(:email) }
     it { should allow_value('liss@ab-codigo.com').for(:email) }
   end
+
+  context 'user_provider_uid' do
+    it 'create if the user not exist' do
+      expect do
+        User.from_omniauth(uid: '12345',
+                           provider: 'facebook',
+                           info: { email: 'liss@abc-codigo.com',
+                                   name: 'Liss' })
+      end.to change(User, :count).by(1)
+    end
+
+    it 'find if the user exist' do
+      user = FactoryBot.create(:user)
+      expect do
+        User.from_omniauth(uid: user.uid,
+                           provider: user.provider,
+                           info: { email: user.email,
+                                   name: user.name })
+      end.to change(User, :count).by(0)
+    end
+
+    it 'return existing user' do
+      user = FactoryBot.create(:user)
+      expect(
+        User.from_omniauth(uid: user.uid, provider: user.provider)
+      ).to eq(user)
+    end
+  end
 end
