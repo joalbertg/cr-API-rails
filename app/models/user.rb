fecd9d7c4adf8a8d6818f001150a6f104a0ca14d
lisset-orozco <liss612@gmail.com>
2019-03-31 14:07:51 -0600

    user_provider_uid from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,24 @@ class User < ActiveRecord::Base
             format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
                       on: :create }
 
-
   # -- Scopes ---------------------------------------------------------------
 
   # -- Callbacks ------------------------------------------------------------
 
   # -- Class Methods --------------------------------------------------------
+  def self.from_omniauth(data)
+    # receive hash
+    # { provider: 'facebook', uid: '12345',
+    #   info: { email: 'liss...', name: 'Liss' } }
+
+    where(provider: data.fetch(:provider),
+          uid: data.fetch(:uid)).first_or_create do |user|
+            if data.fetch(:info)
+              user.email = data.fetch(:info).fetch(:email)
+              # user.name = data.fetch(:info).fetch(:name)
+            end
+          end
+  end
 
   # -- Instance Methods -----------------------------------------------------
 end
