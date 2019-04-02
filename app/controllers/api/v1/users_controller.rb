88d322bf2cbb953374f317cf29bb2bafb2ed153c
lisset-orozco <liss612@gmail.com>
2019-04-02 14:19:03 -0600

    user login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
-# users
 module Api
   module V1
+    # users
     class UsersController < ApplicationController
+      # POST /users
+      def create
+        # params = { auth: { provider: , uid: } }
+        @user = User.from_omniauth(params.fetch(:auth))
+        @token = @user.tokens.create # Token.create(user: @user)
 
+        render 'api/v1/users/show'
+      end
     end
   end
 end
