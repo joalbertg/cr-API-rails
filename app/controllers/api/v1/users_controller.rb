1ca2cd3b683da9936b2dccc5c87f78ff61c5f8c8
lisset-orozco <liss612@gmail.com>
2019-04-03 00:09:01 -0600

    UserController request test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,11 +4,14 @@ module Api
   module V1
     # users
     class UsersController < ApplicationController
+      include ErrorHandler
       # POST /users
       def create
         # params = { auth: { provider: , uid: } }
+        return if param_missing?(:auth)
+
         @user = User.from_omniauth(params.fetch(:auth))
-        @token = @user.tokens.create # Token.create(user: @user)
+        @token = @user.tokens.create
 
         render 'api/v1/users/show'
       end
