d860c094ae55e26fd21acbdf146497593485e1ab
lisset-orozco <liss612@gmail.com>
2019-04-11 19:11:24 -0500

    nested resources

---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Api
+  # version 1
+  module V1
+    # questions controller
+    class QuestionsController < ApiV1Controller
+      before_action :set_question, only: %i[show update destroy]
+
+      # GET /polls/:poll_id/questions
+      def index
+        @questions = @poll.questions
+      end
+
+      # GET /polls/1/questions/2
+      def show; end
+
+      # POST /polls/1/questions
+      def create; end
+
+      # PATCH/PUT /polls/1/questions/1
+      def update; end
+
+      # DELETE /polls/1/questions/1
+      def destroy; end
+    end
+
+    private
+
+    def question_params
+      params.require(:question).permit(:description)
+    end
+
+    def set_question
+      @poll = MyPoll.find_by_id(params[:poll_id])
+      @question = Question.find_by_id(params[:id])
+    end
+  end
+end
--- app/models/my_poll.rb
+++ app/models/my_poll.rb
@@ -3,6 +3,7 @@
 # poll model
 class MyPoll < ActiveRecord::Base
   belongs_to :user
+  has_many :questions
 
   validates :title, presence: true, length: { minimum: 10 }
   validates :description, presence: true, length: { minimum: 20 }
