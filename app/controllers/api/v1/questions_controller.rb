f0e55838127ff199e17dd9c641a1b2803c871112
lisset-orozco <liss612@gmail.com>
2019-04-15 10:39:03 -0500

    validate owner of the poll

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -5,7 +5,8 @@ module Api
   module V1
     # questions controller
     class QuestionsController < ApiV1Controller
-      before_action :set_question, only: %i[show update destroy]
+      before_action :authenticate, only: %i[create update destroy]
+      # before_action :set_question, only: %i[show update destroy]
 
       # GET /polls/:poll_id/questions
       def index
@@ -14,39 +15,52 @@ module Api
       end
 
       # GET /polls/1/questions/2
-      def show; end
+      def show
+        set_question
+      end
 
       # POST /polls/1/questions
       def create
         set_poll
-        @question = @poll.questions.new(question_params)
-        return render 'api/v1/questions/show' if @question.save
 
-        error_message('error', :unprocessable_entity, @question)
+        create_question unless authenticate_owner('user')
       end
 
       # PATCH/PUT /polls/1/questions/1
       def update
+        set_question
         set_poll
       end
 
       # DELETE /polls/1/questions/1
       def destroy
+        set_question
         set_poll
       end
 
       private
 
-      def set_poll
-        @poll = MyPoll.find_by_id(params[:poll_id])
-      end
-
       def question_params
         params.require(:question).permit(:description)
       end
 
+      def set_poll
+        @poll = MyPoll.find_by_id(params[:poll_id])
+      end
+
       def set_question
         @question = Question.find_by_id(params[:id])
+      end
+
+      def authenticate_owner(type)
+        @poll.user != @current_user ? error_message(type, :unauthorized) : false
+      end
+
+      def create_question
+        @question = @poll.questions.new(question_params)
+        return render 'api/v1/questions/show' if @question.save
+
+        error_message('error', :unprocessable_entity, @question)
       end
     end
   end
