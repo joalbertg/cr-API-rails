299365304f8761bf256663ec060d77a66765a6d3
lisset-orozco <liss612@gmail.com>
2019-05-07 17:36:16 -0500

    get logged in user

---
+++ app/controllers/concerns/user_authentication.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# user authentication
+module UserAuthentication
+  extend ActiveSupport::Concern
+
+  def user_signed_in?
+    # true if the user is logged in otherwise false
+    !current_user.nil?
+  end
+
+  def current_user
+    # nil or logged in user
+    User.find_by_id(session[:user_id])
+  end
+end
